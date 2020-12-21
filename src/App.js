200546f922275f2a7d39af4c35bafc2bdb53c081
Lateralus <zoofoo.online@gmail.com>
2020-12-21 18:43:23 +0000

    app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Products from './Products';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  formatName(user) {
+    return user.firstName + '' + user.lastName;
+  }
+
+  render() {
+    const user = {
+      firstName: 'Greg',
+      lastName: 'Lim',
+    };
+
+    return (
+      <div>
+        <h1>Hello, {this.formatName(user)}</h1>
+        <Products />
+      </div>
+    );
+  }
 }
 
 export default App;
