b38a49a07bf47c4f2107d6d783a03cb2c9f00e77
Lateralus <zoofoo.online@gmail.com>
2021-01-08 02:21:23 +0000

    mapped through an object to get product rating

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import { Button } from 'react-bootstrap';
-import Rating from './Rating';
 
 class App extends Component {
-  formatName(user) {
-    return user.firstName + '' + user.lastName;
-  }
-
   render() {
-    const isValid = true;
-
-    const user = {
-      firstName: 'Greg',
-      lastName: 'Lim',
-    };
-
     return (
       <div>
-        <h1>Hello, {this.formatName(user)}</h1>
         <Products />
-        <Button variant="danger" disabled={!isValid}>
-          Default
-        </Button>
-        <div>
-          <Rating rating="1" />
-          <Rating rating="2" />
-          <Rating rating="3" />
-          <Rating rating="4" />
-          <Rating rating="5" />
-        </div>
       </div>
     );
   }
